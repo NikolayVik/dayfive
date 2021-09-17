47617b78f1c2ec20b8d7a9aa5b476ef8ee96196b
Nikolay <tabashin@yandex.ru>
2021-09-17 14:16:59 +0300

    bird1

---
+++ ptica/ptica/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ptica
+//
+//  Created by user on 16.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+        Color("ColorBlue")
+        }.ignoresSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
